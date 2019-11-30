ecf71e9c094c2f9426ee311bac85afd0c26e6b98
50vincents <50vincents@github.com>
2019-11-30 12:46:16 -0800

    working unit tests

---
+++ QuietPlace/__tests__/vince-test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import UserSignUp from '../userSignUp';
+
+const createTestProps = (props: Object) => ({
+  navigation: {
+    navigate: jest.fn()
+  },
+  ...props
+});
+
+describe('User sign up page', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    let props: any;
+    props = createTestProps({});
+    wrapper = mount(<UserSignUp {...props}/>);
+  });
+
+  it('should return an error if there is no user input', () => {
+    expect(wrapper.state('email')).toBe('');
+    expect(wrapper.state('password')).toBe('');
+    wrapper.find('#submitButton').simulate('click');
+    expect(wrapper.state('errorMessage')).toBe('Error: empty input(s)');
+  });
+
+  it('should update the state if email and password are provided', () => {
+    wrapper.setState({email: 'blah@blah.com'});
+    wrapper.setState({password: 'hasija'});
+
+    expect(wrapper.state('email')).toBe('blah@blah.com');
+    expect(wrapper.state('password')).toBe('hasija');
+  });
+});
